63b06ee243589d2a61aaf6ca2018eb5f2d5b05a8
sohaib757 <1000065@spsd.sk.ca>
2025-11-18 09:53:14 -0600

    very basic path

--- sketch.js
+++ sketch.js
@@ -10,6 +10,10 @@ class Enemy {
     this.x = x;
     this.y = y;
   }
+  
+  display() {
+    circle(this.x, this.y, 15);
+  }
 }
 
 function setup() {
@@ -18,7 +22,17 @@ function setup() {
 
 function draw() {
   background(220);
+  generatePath();
 }
 
 function generatePath() {
+  for (let i = 20; i < 40; i++) {
+    line(i, 0, i, height/2);
+  }
+  for (let i = 20; i < width/2; i++) {
+    line(i, height/2, i, height/2 + 20);
+  }
+  for (let i = width/2; i < width/2 + 20; i++) {
+    line(i, height/2 - 20 + 20, i, height);
+  }
 }
