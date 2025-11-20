1f4fc1d5374a832db0610a0469924c4b9a485b9c
sohaib757 <1000065@spsd.sk.ca>
2025-11-20 09:34:40 -0600

    spawn enemies

--- sketch.js
+++ sketch.js
@@ -12,26 +12,53 @@ class Enemy {
   }
   
   display() {
+    noStroke();
+    fill("green");
     circle(this.x, this.y, 15);
+  }
+  
+  move() {
+    this.x += 1;
+    this.y += 1;
   }
 }
 
 let pathPoints = [
   {x: 40, y: 0},
   {x: 40, y: 400},
+  {x: 500, y: 400},
+  {x: 500, y: 600},
+  {x: 800, y: 600},
+  {x: 800, y: 200},
+  {x: 1000, y: 200}
 ];
+
+let enemies = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for (let n = 0; n < 10; n ++) {
+    let aEnemy = new Enemy(pathPoints[0].x, pathPoints[0].y);
+    enemies.push(aEnemy);
+  }
 }
 
 function draw() {
   background(220);
   generatePath();
+  for (let enemy of enemies) {
+    if (enemy.y < pathPoints[1].y) {
+      enemy.move();
+    }
+    enemy.display();
+  }
 }
 
 function generatePath() {
+  stroke("black");
   strokeWeight(50);
-  line(pathPoints[0].x, pathPoints[0].y, pathPoints[1].x, pathPoints[1].y);
+  for (let i = 0; i < 6; i++) {
+    line(pathPoints[i].x, pathPoints[i].y, pathPoints[i+1].x, pathPoints[i+1].y);
+  }
 }
 
